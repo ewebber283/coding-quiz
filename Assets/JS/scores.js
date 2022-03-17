de9c01169a49605ee79bdacec0f9d67c1f8ba98a
Eric Webber <ewebber283@gmail.com>
2022-03-17 16:55:55 -0500

    debugging js issues with showing highscores

--- Assets/JS/scores.js
+++ Assets/JS/scores.js
@@ -1,6 +1,6 @@
 const highScoresList = document.querySelector('#highScoresList');
-const highscores = JSON.parse(localStorage.getItem('highScores')) || {};
+const highScores = JSON.parse(localStorage.getItem('highScores')) || {};
 
 highScoresList.innerHTML = highScores.map(score => {
-    return`<li class="high-score">${score.name} - ${score.score}</li>`
+    return `<li class="high-score">${score.name} - ${score.score}</li>`
 }).join('')
